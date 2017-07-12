c7a4c03f0d39ad946ebf94f663ce84751832f5bc
Waiting7777 <waiting0819@msn.com>
2017-07-13 00:45:08 +0800

    Game Angry Chicken add collison group

--- angry-chickens/js/states/Game.js
+++ angry-chickens/js/states/Game.js
@@ -12,6 +12,10 @@ Achicken.GameState = {
 
         this.game.physics.p2.gravity.y = 1000
 
+        this.blocksCollisionGroup = this.game.physics.p2.createCollisionGroup()
+        this.enemiesCollisionGroup = this.game.physics.p2.createCollisionGroup()
+        this.chickensCollisionGroup = this.game.physics.p2.createCollisionGroup()
+
     },
     create: function(){
 
@@ -25,11 +29,23 @@ Achicken.GameState = {
         this.floor = this.add.tileSprite(this.game.world.width/2, this.game.world.height - 24, this.game.world.width, 48, 'floor')
         this.blocks.add(this.floor)
 
+        this.floor.body.setCollisionGroup(this.blocksCollisionGroup)
+
+        this.floor.body.collides([this.blocksCollisionGroup, this.enemiesCollisionGroup])
+
+        this.floor.body.static = true
+
         this.box = this.add.sprite(100, 100, 'box')
         this.blocks.add(this.box)
 
-        this.box = this.add.sprite(120, 200, 'box')
-        this.blocks.add(this.box)
+        this.box.body.setCollisionGroup(this.blocksCollisionGroup)
+        this.box.body.collides(this.blocksCollisionGroup)
+
+        var box2 = this.add.sprite(120, 200, 'box')
+        this.blocks.add(box2)
+
+        box2.body.setCollisionGroup(this.blocksCollisionGroup)
+        box2.body.collides(this.blocksCollisionGroup)
 
     },
     update: function(){
