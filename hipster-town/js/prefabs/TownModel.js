627f886e0fa8b353c15a17f701e52c120f557c67
Waiting7777 <waiting0819@msn.com>
2017-07-17 12:47:18 +0800

    Game Hipster Town add food model

--- hipster-town/js/prefabs/TownModel.js
+++ hipster-town/js/prefabs/TownModel.js
@@ -4,6 +4,7 @@ HTown.TownModel = function(coefs, initialStats, building){
 
     this.coefs = {}
     this.coefs.populationGrowth = this.coefs.populationGrowth || 1.02
+    this.coefs.foodConsumption = this.coefs.foodConsumption || 5
 
     this.stats = {}
     this.stats.population = initialStats.population
@@ -19,6 +20,14 @@ HTown.TownModel.prototype.step = function(){
     this.stats.population = this.stats.population * this.coef.populationGrowth
 
     this.stats.population = Math.min(this.stats.population, this.stats.housing)
+
+    this.stats.food -= this.stats.population * this.coefs.foodConsumption
+
+    if(this.stats.food < 0){
+        this.stats.population += this.stats.food / this.coefs.foodConsumption
+        this.stats.food = 0
+    }
+
 }
 
 HTown.TownModel.prototype.updateBuildingProduction = function(){
@@ -28,5 +37,10 @@ HTown.TownModel.prototype.updateBuildingProduction = function(){
         if(building.housing){
             this.stats.housing += building.housing
         }
+
+        if(building.food){
+            this.stats.food += building.food
+        }
+
     }, this)
 }
