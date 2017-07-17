30af068b6c8a11c28f0196201663788ed6fd14a0
Waiting7777 <waiting0819@msn.com>
2017-07-17 13:02:21 +0800

    Game Hipster Town add job model

--- hipster-town/js/prefabs/TownModel.js
+++ hipster-town/js/prefabs/TownModel.js
@@ -1,15 +1,18 @@
 var HTown = HTown || {}
 
-HTown.TownModel = function(coefs, initialStats, building){
+HTown.TownModel = function(coefs, initialStats, buildings){
 
     this.coefs = {}
     this.coefs.populationGrowth = this.coefs.populationGrowth || 1.02
     this.coefs.foodConsumption = this.coefs.foodConsumption || 5
+    this.coefs.productivityPerPerson = this.coefs.productivityPerPerson || 0.5
 
     this.stats = {}
     this.stats.population = initialStats.population
     this.stats.food = initialStats.food
     this.stats.money = initialStats.money
+
+    this.buildings = buildings
 
 }
 
@@ -17,7 +20,7 @@ HTown.TownModel.prototype.step = function(){
 
     this.updateBuildingProduction()
 
-    this.stats.population = this.stats.population * this.coef.populationGrowth
+    this.stats.population = this.stats.population * this.coefs.populationGrowth
 
     this.stats.population = Math.min(this.stats.population, this.stats.housing)
 
@@ -28,14 +31,23 @@ HTown.TownModel.prototype.step = function(){
         this.stats.food = 0
     }
 
+    this.stats.money += Math.min(this.stats.population, this.stats.jobs) * this.coefs.productivityPerPerson
+
+    console.log(this.stats)
+
 }
 
 HTown.TownModel.prototype.updateBuildingProduction = function(){
     this.stats.housing = 0
+    this.stats.jobs = 0
 
     this.buildings.forEach(function(building){
         if(building.housing){
             this.stats.housing += building.housing
+        }
+
+        if(building.jobs){
+            this.stats.jobs += building.jobs
         }
 
         if(building.food){
