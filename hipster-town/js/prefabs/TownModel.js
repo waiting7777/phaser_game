9caa9bcece3d16580885d3d33c34ff402520543d
Waiting7777 <waiting0819@msn.com>
2017-07-17 12:11:29 +0800

    Game Town Hipster add town model

--- hipster-town/js/prefabs/TownModel.js
+++ hipster-town/js/prefabs/TownModel.js
@@ -1,5 +1,17 @@
 var HTown = HTown || {}
 
-HTown.TownModel = function(){
+HTown.TownModel = function(initialStats){
 
+    this.coefs = {}
+    this.coefs.populationGrowth = this.coefs.populationGrowth || 1.02
+
+    this.stats = {}
+    this.stats.population = initialStats.population
+    this.stats.food = initialStats.food
+    this.stats.money = initialStats.money
+
+}
+
+HTown.TownModel.prototype.step = function(){
+    this.stats.population = this.stats.population * this.coef.populationGrowth
 }
