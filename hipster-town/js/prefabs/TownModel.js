f662a8be1ba85b62cdbcd1270d5e6421d8a836c5
Waiting7777 <waiting0819@msn.com>
2017-07-17 12:30:07 +0800

    Game Hipster Town add housing model

--- hipster-town/js/prefabs/TownModel.js
+++ hipster-town/js/prefabs/TownModel.js
@@ -1,6 +1,6 @@
 var HTown = HTown || {}
 
-HTown.TownModel = function(initialStats){
+HTown.TownModel = function(coefs, initialStats, building){
 
     this.coefs = {}
     this.coefs.populationGrowth = this.coefs.populationGrowth || 1.02
@@ -13,5 +13,20 @@ HTown.TownModel = function(initialStats){
 }
 
 HTown.TownModel.prototype.step = function(){
+
+    this.updateBuildingProduction()
+
     this.stats.population = this.stats.population * this.coef.populationGrowth
+
+    this.stats.population = Math.min(this.stats.population, this.stats.housing)
+}
+
+HTown.TownModel.prototype.updateBuildingProduction = function(){
+    this.stats.housing = 0
+
+    this.buildings.forEach(function(building){
+        if(building.housing){
+            this.stats.housing += building.housing
+        }
+    }, this)
 }
