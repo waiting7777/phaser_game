346775236fcd02d5670b3d6a526535b80434e12f
Waiting7777 <waiting0819@msn.com>
2017-08-07 02:03:14 +0800

    Game Dungeon Crawler add get surrounding method

--- dungeon-crawler/js/prefabs/Board.js
+++ dungeon-crawler/js/prefabs/Board.js
@@ -13,10 +13,42 @@ DunCrawl.Board = function(state, data){
     for(i = 0; i < this.rows; i++){
         for(j = 0; j < this.cols; j++){
             tile = new Phaser.Sprite(this.game, j * this.tileSize, i * this.tileSize, 'rockTile')
-            tile.row = j
-            tile.col = i
+            tile.row = i
+            tile.col = j
             this.state.backgroundTiles.add(tile)
+
+            tile.inputEnabled = true
+            tile.events.onInputDown.add(function(tile){
+                tile.alpha = 0.5
+                console.log('row: ' + tile.row + ' col: ' + tile.col)
+                console.log(this.getSurrounding(tile))
+            }, this)
         }
     }
 
+}
+
+DunCrawl.Board.prototype.getSurrounding = function(tile){
+    var adjacentTiles = []
+    var relativePositions = [
+        {r: 1, c: -1},
+        {r: 1, c: 0},
+        {r: 1, c: 1},
+        {r: 0, c: -1},
+        {r: 0, c: 1},
+        {r: -1, c: -1},
+        {r: -1, c: 0},
+        {r: -1, c: 1}
+    ]
+
+    relativePositions.forEach(function(relPos){
+        relRow = tile.row + relPos.r
+        relCol = tile.col + relPos.c
+
+        if(relRow >= 0 && relRow < this.rows && relCol >= 0 && relCol < this.cols){
+            adjacentTiles.push({row: relRow, col: relCol})
+        }
+    }, this)
+
+    return adjacentTiles
 }
